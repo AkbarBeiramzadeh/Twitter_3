1e177ae24f11f9d651f454ebd774b39e1e50b2fa
AkbarBeiramzadeh <akbar.beiramzadeh@gmail.com>
2023-07-29 09:11:11 +0330

    feat:added class User and Relation

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.utils.translation import gettext as _
 
-# Create your models here.
+
+class User(AbstractUser):
+    email = models.EmailField(
+        verbose_name=_("email address"),
+        unique=True,
+    )
+    bio = models.CharField(
+        verbose_name=_('biography'),
+        max_length=100,
+        null=True,
+        blank=True,
+    )
+    image = models.ImageField(
+        verbose_name=_('image'),
+        upload_to='user/images/',
+        null=True,
+        blank=True,
+    )
+
+
+class Relation(models.Model):
+    from_user = models.ForeignKey(User,on_delete=models.CASCADE,related_name="followings",)
+    to_user = models.ForeignKey(User,on_delete=models.CASCADE,related_name="followers",)
+
+    class Meta:
+        verbose_name = _("Relation")
+        verbose_name_plural = _("Relations")
