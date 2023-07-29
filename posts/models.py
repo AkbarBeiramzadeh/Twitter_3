ffc19b197dc33b22abc4dfbecb53aba346bb2bbf
AkbarBeiramzadeh <akbar.beiramzadeh@gmail.com>
2023-07-29 11:47:18 +0330

    feat: added class Tag

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.urls import reverse
+from django.utils.translation import gettext_lazy as _
+from core.models import (
+    BaseModel,
+    SoftDeleteModel,
+    TimeStampMixin,
+)
+
 
 # Create your models here.
+class Tag(BaseModel):
+    text = models.CharField(max_length=10)
+
+    def __str__(self) -> str:
+        return self.text
+
+
