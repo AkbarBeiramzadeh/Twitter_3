1b78a8cb374a0412c2eea8d979108f47525ef6c3
AkbarBeiramzadeh <akbar.beiramzadeh@gmail.com>
2023-07-29 11:46:58 +0330

    feat: added SoftDeleteModel

--- core/models.py
+++ core/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from uuid import uuid4
+from django.db.models.query import QuerySet
 
 
 class BaseModel(models.Model):
@@ -12,3 +13,24 @@ class BaseModel(models.Model):
 class TimeStampMixin:
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+
+class MyManager(models.Manager):
+    def get_queryset(self) -> QuerySet:
+        return super().get_queryset().filter(is_deleted=False)
+
+    def archives(self):
+        return super().get_queryset().filter(is_deleted=True)
+
+
+class SoftDeleteModel(BaseModel):
+    objects = MyManager()
+
+    is_deleted = models.BooleanField(default=False, db_index=True)
+
+    def delete(self, **kwargs):
+        self.is_deleted = True
+        self.save()
+
+    class Meta:
+        abstract = True
